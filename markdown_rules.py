9c99b8fe839ca09ea715bcceb93ca4b3f80481bf
T9Air <163940743+T9Air@users.noreply.github.com>
2024-10-21 23:13:31 -0400

    Update markdown_rules.py
    headings

--- markdown_rules.py
+++ markdown_rules.py
@@ -1,5 +1,11 @@
 import re
 
 search_rules = {
-    ("newline", "r'\n'")
+    "newline": (r"\n", "(newline)"),
+    "heading1": (r"^# (.+)$", "(h1)\\1(h1)"),
+    "heading2": (r"^## (.+)$", "(h2)\\1(h2)"),
+    "heading3": (r"^### (.+)$", "(h3)\\1(h3)"),
+    "heading4": (r"^#### (.+)$", "(h4)\\1(h4)"),
+    "heading5": (r"^##### (.+)$", "(h5)\\1(h5)"),
+    "heading6": (r"^###### (.+)$", "(h6)\\1(h6)")
 }
