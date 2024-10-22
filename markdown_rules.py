1ece54da8da4fed4abb1413aa972236d65b1a0c6
T9Air <163940743+T9Air@users.noreply.github.com>
2024-10-22 11:50:11 -0400

    Update markdown_rules.py
    bold before italic

--- markdown_rules.py
+++ markdown_rules.py
@@ -7,8 +7,8 @@ search_rules = {
     # "heading3": (r"^### (.+)$", "(h3)\\1(h3)"),
     # "heading2": (r"^## (.+)$", "(h2)\\1(h2)"),
     # "heading1": (r"^# (.+)$", "(h1)\\1(h1)"),
-    "italic": (r"\*(.+)\*", "(i)\\1(i)"),
     "bold": (r"\*\*(.+)\*\*", "(b)\\1(b)"),
+    "italic": (r"\*(.+)\*", "(i)\\1(i)"),
     "blockquote": (r"^> (.+)$", "(bq)\\1(bq)"),
     "orderedlist": (r"^\d+\. (.+)$", "(ol)\\1(ol)"),
     "unorderedlist": (r"^\ - (.+)$", "(ul)\\1(ul)"),
@@ -20,4 +20,5 @@ search_rules = {
 def parse_markdown(markdown_text):
     for rule_name, (pattern, replacement) in search_rules.items():
         markdown_text = re.sub(pattern, replacement, markdown_text)
+    print(markdown_text)
     return markdown_text
