00eb9bf4b8d7fe61484d71f99ed269dcc349e48f
T9Air <163940743+T9Air@users.noreply.github.com>
2024-10-22 18:24:38 -0400

    Revert "Update markdown_rules.py"
    This reverts commit eaf22e59bb0541eb31152a7ced4534693d968600.

--- markdown_rules.py
+++ markdown_rules.py
@@ -1,21 +1,21 @@
 import re
 
 search_rules = {
-    "heading6": (r"^###### (.+)$", "<h6>\\1</h6>"),
-    "heading5": (r"^##### (.+)$", "<h5>\\1</h5>"),
-    "heading4": (r"^#### (.+)$", "<h4>\\1</h4>"),
-    "heading3": (r"^### (.+)$", "<h3>\\1</h3>"),
-    "heading2": (r"^## (.+)$", "<h2>\\1</h2>"),
-    "heading1": (r"^# (.+)$", "<h1>\\1</h1>"),
-    "bold&italic": (r"\*\*\*(.+)\*\*\*", "<b><i>\\1</i></b>"),
-    "bold": (r"\*\*(.+)\*\*", "<b>\\1</b>"),
-    "italic": (r"\*(.+)\*", "<i>\\1</i>"),
-    "blockquote": (r"^> (.+)$", "<blockquote>\\1</blockquote>"),
-    #"orderedlist": (r"^\d+\. (.+)$", "(ol)\\1(ol)"),
-    #"unorderedlist": (r"^\ - (.+)$", "(ul)\\1(ul)"),
-    "link": (r"\[(.+)]\((.+)\)", '<a href="\\2">\\1</a>'),
-    "image": (r"!\[(.+)]\((.+)\)", '<img src="\\2" alt="\\1">'),
-    "newline": (r"\n", "<br>")    
+    "heading6": (r"^###### (.+)$", "(h6)\\1(h6"),
+    "heading5": (r"^##### (.+)$", "(h5)\\1(h5)"),
+    "heading4": (r"^#### (.+)$", "(h4)\\1(h4)"),
+    "heading3": (r"^### (.+)$", "(h3)\\1(h3)"),
+    "heading2": (r"^## (.+)$", "(h2)\\1(h2)"),
+    "heading1": (r"^# (.+)$", "(h1)\\1(h1)"),
+    "bold&italic": (r"\*\*\*(.+)\*\*\*", "(bi)\\1(bi)"),
+    "bold": (r"\*\*(.+)\*\*", "(b)\\1(b)"),
+    "italic": (r"\*(.+)\*", "(i)\\1(i)"),
+    "blockquote": (r"^> (.+)$", "(bq)\\1(bq)"),
+    "orderedlist": (r"^\d+\. (.+)$", "(ol)\\1(ol)"),
+    "unorderedlist": (r"^\ - (.+)$", "(ul)\\1(ul)"),
+    "link": (r"\[(.+)]\((.+)\)", "(link)(text)\\1(text)(address)\\2(address)(link)"),
+    "image": (r"!\[(.+)]\((.+)\)", "(image)(text)\\1(text)(address)\\2(address)(image)"),
+    "newline": (r"\n", "(newline)")    
 }
 
 def parse_markdown(markdown_text):
