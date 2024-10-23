4c4ce42286869712d23e96df35ee4dc64359351f
T9Air <163940743+T9Air@users.noreply.github.com>
2024-10-22 20:03:07 -0400

    Update converter.py
    output the parsed text to the textbox without (newline)

--- converter.py
+++ converter.py
@@ -3,4 +3,6 @@ import re
 import tkinter as tk
 
 def parsed_to_readable(parsed_text, textbox):
-    parsed_text = parsed_text
+    split_text = parsed_text.split("(newline)")
+    for line in split_text:
+        textbox.insert(tk.END, line + "\n")
