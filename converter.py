c9cccf636394e210638f5d9f23518ce13dd51d94
T9Air <163940743+T9Air@users.noreply.github.com>
2024-10-30 22:41:22 -0400

    Update converter.py

--- converter.py
+++ converter.py
@@ -5,53 +5,53 @@ import tkinter as tk
 def parsed_to_readable(parsed_text, textbox):
     split_text = parsed_text.split("(newline)")
     for line in split_text:
-        font_size = None
+        # font_size = None
         
-        # Headings
-        heading_level = None
-        for i in range(1, 7):
-            if line.startswith(f"(h{i})"):
-                heading_level = i
-                break
+        # # Headings
+        # heading_level = None
+        # for i in range(1, 7):
+        #     if line.startswith(f"(h{i})"):
+        #         heading_level = i
+        #         break
         
-        if heading_level:
-            font_size = {
-                1: 18,
-                2: 14,
-                3: 12,
-                4: 10,
-                5: 9,
-                6: 8
-            }.get(heading_level, 10)
-            line = line[4:]
+        # if heading_level:
+        #     font_size = {
+        #         1: 18,
+        #         2: 14,
+        #         3: 12,
+        #         4: 10,
+        #         5: 9,
+        #         6: 8
+        #     }.get(heading_level, 10)
+        #     line = line[4:]
         
-        if heading_level:
-            heading = "h"
-            bold = "bold"
-        else:
-            heading = "r"
-            font_size = 10
-            bold = ""
+        # if heading_level:
+        #     heading = "h"
+        #     bold = "bold"
+        # else:
+        #     heading = "r"
+        #     font_size = 10
+        #     bold = ""
         
-        # Bold
-        bold_indices = []
-        bold_characters = []
-        matches = re.finditer(r"(\(b\))(.+)(\(b\))", line)
+        # # Bold
+        # bold_indices = []
+        # bold_characters = []
+        # matches = re.finditer(r"(\(b\))(.+)(\(b\))", line)
         
-        for match in matches:
-            start, end = match.span()
-            bold_indices.append((start, end))
-            bold_characters.extend([i for i in range(start + 1, end + 1)])
+        # for match in matches:
+        #     start, end = match.span()
+        #     bold_indices.append((start, end))
+        #     bold_characters.extend([i for i in range(start + 1, end + 1)])
             
-        # Italic
-        italic_indices = []
-        italic_characters = []
-        matches = re.finditer(r"(\(i\))(.+)(\(i\))", line)
+        # # Italic
+        # italic_indices = []
+        # italic_characters = []
+        # matches = re.finditer(r"(\(i\))(.+)(\(i\))", line)
         
-        for match in matches:
-            start, end = match.span()
-            italic_indices.append((start, end))
-            italic_characters.extend([i for i in range(start + 1, end + 1)])
+        # for match in matches:
+        #     start, end = match.span()
+        #     italic_indices.append((start, end))
+        #     italic_characters.extend([i for i in range(start + 1, end + 1)])
 
         # Adding characters to textbox
         char_num = 0 # Create a variable to store what number character on the line it is up to
