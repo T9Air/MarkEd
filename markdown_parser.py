be41dfb53b232353e3158e8be1b116b60fed3363
T9Air <163940743+T9Air@users.noreply.github.com>
2024-11-02 22:45:39 -0400

    Update markdown_parser.py
    italics work other than escaping a *

--- markdown_parser.py
+++ markdown_parser.py
@@ -7,13 +7,31 @@ markdown_rules = {
     "heading3": (r"^### (.+)$", "(h3)\\1"),
     "heading2": (r"^## (.+)$", "(h2)\\1"),
     "heading1": (r"^# (.+)$", "(h1)\\1"),
-    "bold": (r"\*\*(.+)\*\*", "(b)\\1(b)"),
     "inline_code": (r"\`(.+)\`", "(ic)\\1(ic)"),
     "unordered_list": (r"^\ - (.+)$", "(ul)\\1(ul)"),
     "newline": (r"\n", "(newline)")
 }
 
 def parse_markdown(markdown_text):
+    # Italic
+    lines = markdown_text.splitlines()
+    processed = []
+    
+    for line in lines:
+        astericks = -1
+        i = 0
+        while i < len(line):
+            if line[i] == "*" and astericks == -1:
+                astericks = i
+            elif line[i] == "*" and astericks >= 0:
+                line = line[:i] + "(i)" + line[i + 1:]
+                line = line[:astericks] + "(i)" + line[astericks + 1:]
+                astericks = -1
+            i = i + 1
+        processed.append(line)
+
+    markdown_text = "\n".join(processed)
+    
     for rule_name, (pattern, replacement) in markdown_rules.items():
         markdown_text = re.sub(pattern, replacement, markdown_text, flags=re.MULTILINE)
     return markdown_text
