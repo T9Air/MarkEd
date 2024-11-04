3df59bd95cc929e1f94cd226dc00ed13c10e8c62
T9Air <163940743+T9Air@users.noreply.github.com>
2024-11-04 06:33:13 -0500

    Revert "UI Update 0.0.0.0.2 (Animations)"
    This reverts commit 13198abd1be054248a38aa23c916f4d0e407f3f2.
    I prefer if the ui is simple, which will make it easier for people to use

--- main.py
+++ main.py
@@ -6,27 +6,22 @@ from markdown_parser import parse_markdown
 
 from converter import parsed_to_readable
 
-import time
-
 # Root window configuration
 root = tk.Tk()
 
-root.title("MarkEd Desktop")
+root.title("MarkEd")
 #root.state('zoomed')
 root.configure(bg='gray15')
 
-
-
-
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=1)
-
-
 
 # Top frame - save, open file, etc.
 top_frame = tk.Frame(root, height=1, bg='gray15')
 top_frame.pack(fill='x', padx=10, pady=10)
 
+top_frame.columnconfigure(0, weight=1)
+top_frame.columnconfigure(1, weight=1)
 
 file_path = ""
 
@@ -58,16 +53,18 @@ def open_file():
             realtext_box.config(state="disabled")
         file_path = open_file_path
 
-open_btn = tk.Button(top_frame, text="Open file", command=open_file, relief='flat', overrelief='solid')
-open_btn.grid(row=0, column=0, padx=5)
+open_btn = tk.Button(top_frame, text="Open file", height=1, command=open_file, relief='flat', overrelief='solid')
+open_btn.grid(row=0, column=0, padx=5, sticky='w')
 
-save_btn = tk.Button(top_frame, text="Save file", command=save, relief='flat', overrelief='solid')
-save_btn.grid(row=0, column=1, padx=5)
+save_btn = tk.Button(top_frame, text="Save file", height=1, command=save, relief='flat', overrelief='solid')
+save_btn.grid(row=0, column=1, padx=5, sticky='w')
 
 # Markdown frame - markdown text
-markdown_frame = tk.Frame(root, bg='grey30')
+markdown_frame = tk.Frame(root, bg='red')
 markdown_frame.pack(side='left', fill='both', expand=True, padx=5, pady=5)
 
+markdown_frame.columnconfigure(0, weight=1)
+markdown_frame.rowconfigure(0, weight=1)
 
 def update_text(event=None):
     def delayed_update():
@@ -82,82 +79,17 @@ def update_text(event=None):
     root.after(1, delayed_update)
 
 markdown_box = tk.Text(markdown_frame, height=30, width=90, yscrollcommand=True, bg='gray30', fg='white')
-markdown_box.pack(fill='both', expand=True, padx=1, pady=1)
+markdown_box.grid(row=0, column=0, sticky="nsew")
 markdown_box.bind("<KeyPress>", update_text)
 
 # Realtext frame - converted text
-realtext_frame = tk.Frame(root, bg='grey30')
+realtext_frame = tk.Frame(root)
 realtext_frame.pack(side='right', fill='both', expand=True, padx=5, pady=5)
 
-
+realtext_frame.columnconfigure(0, weight=1)
+realtext_frame.rowconfigure(0, weight=1)
 
 realtext_box = tk.Text(realtext_frame, height=30, width=90, yscrollcommand=True, bg='gray30', fg='white')
-realtext_box.pack(fill='both', expand=True)
-
-
-
-y=100
-for x in range(1, 70):
-    y-=1
-    markdown_box.config(bg=f'gray{y}')
-    realtext_box.config(bg=f'gray{y}')
-    root.update()
-    time.sleep(.03)
-
-
-# Rando Animation
-root.title('M')
-root.update()
-time.sleep(.05)
-
-root.title('Ma')
-root.update()
-time.sleep(.05)
-
-root.title('Mar')
-root.update()
-time.sleep(.05)
-
-root.title('Mark')
-root.update()
-time.sleep(.05)
-
-root.title('MarkE')
-root.update()
-time.sleep(.05)
-
-root.title('MarkEd')
-root.update()
-time.sleep(.05)
-
-root.title('MarkEd D')
-root.update()
-time.sleep(.05)
-
-root.title('MarkEd De')
-root.update()
-time.sleep(.05)
-
-root.title('MarkEd Des')
-root.update()
-time.sleep(.05)
-
-root.title('MarkEd Desk')
-root.update()
-time.sleep(.05)
-
-root.title('MarkEd Deskt')
-root.update()
-time.sleep(.05)
-
-root.title('MarkEd Deskto')
-root.update()
-time.sleep(.05)
-
-root.title('MarkEd Desktop')
-root.update()
-time.sleep(.05)
-
-
+realtext_box.grid(row=0, column=0, sticky="nsew")
 
 root.mainloop()
