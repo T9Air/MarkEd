dec19044ac3f62ee21fb226752c25b557660ccba
T9Air <163940743+T9Air@users.noreply.github.com>
2024-11-20 22:16:31 -0500

    Update database_host.py

--- database_host.py
+++ database_host.py
@@ -4,14 +4,12 @@ import sys
 
 def resource_path(relative_path):
     """Get absolute path to resource, works for dev and for PyInstaller"""
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
+    base_path = getattr(sys, '_MEIPASS', os.path.abspath(".")) if hasattr(sys, '_MEIPASS') else os.path.abspath(".")
     return os.path.join(base_path, relative_path)
 
-conn = sqlite3.connect(resource_path('info.db'))
-c = conn.cursor()
+
+Conn = sqlite3.connect(resource_path('info.db'))
+c = Conn.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS settings (
           name_of_setting text,
@@ -24,7 +22,7 @@ def get_setting(setting_name):
 
 def setting_configure(setting_name, change_to):
     c.execute('''UPDATE settings SET on_off='{}' WHERE name_of_setting='{}' '''.format(change_to, setting_name))
-    conn.commit()
+    Conn.commit()
 
 #c.execute("INSERT INTO settings (name_of_setting, on_off) VALUES ('theme', 'dark')")
 
