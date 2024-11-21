04c8e623cdff44abf91e6fb5b5f5655081830b61
T9Air <163940743+T9Air@users.noreply.github.com>
2024-11-21 06:22:07 -0500

    Update database_host.py
    fix sql injection

--- database_host.py
+++ database_host.py
@@ -4,7 +4,8 @@ import sys
 
 def resource_path(relative_path):
     """Get absolute path to resource, works for dev and for PyInstaller"""
-    base_path = getattr(sys, '_MEIPASS', os.path.abspath(".")) if hasattr(sys, '_MEIPASS') else os.path.abspath(".")
+    base_path = (getattr(sys, '_MEIPASS', os.path.abspath("."))
+                 if hasattr(sys, '_MEIPASS') else os.path.abspath("."))
     return os.path.join(base_path, relative_path)
 
 
@@ -16,12 +17,12 @@ c.execute('''CREATE TABLE IF NOT EXISTS settings (
           on_off text)''')
 
 def get_setting(setting_name):
-    c.execute("SELECT on_off FROM settings WHERE name_of_setting='{}'".format(setting_name))
+    c.execute("SELECT on_off FROM settings WHERE name_of_setting=?", (setting_name,))
     grabber = c.fetchone()
     return grabber[0]
 
 def setting_configure(setting_name, change_to):
-    c.execute('''UPDATE settings SET on_off='{}' WHERE name_of_setting='{}' '''.format(change_to, setting_name))
+    c.execute("UPDATE settings SET on_off=? WHERE name_of_setting=?", (change_to, setting_name))
     Conn.commit()
 
 #c.execute("INSERT INTO settings (name_of_setting, on_off) VALUES ('theme', 'dark')")
